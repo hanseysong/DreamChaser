4f509cabd7c65a5e9c38663acf07d17f1756ddf1
hanseysong <93908182+hanseysong@users.noreply.github.com>
2022-06-17 02:57:28 +0200

    toggling

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,10 +1,50 @@
 <script>
-	export let name;
+import { get_current_component } from "svelte/internal";
+
+
+	let firstName = 'ourWork';
+	let lastName = 'group5';
+	let color = 'blue'
+	let showText = false
+	let users = [
+		{
+			id: '1',
+			name: 'sarah'
+		},
+		{
+			id: '2',
+			name: 'john'
+		},
+		{
+			id: '3',
+			name: 'kiwi'
+		},
+	]
+
+	$: name = firstName + " " + lastName
+
+	const toggle = () => {
+		if (color !== 'blue') {
+			color = 'blue'}
+		else {color = 'green'}
+		showText = !showText
+
+	}
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color: {color};">Hello {name}!</h1>
+	{#if showText}
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>No text</p>
+	{/if}
+	<button on:click={toggle}>Click here</button>
+
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
 </main>
 
 <style>
