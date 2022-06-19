7318229a27b0fb299e3791448bf8931a44c18c63
hanseysong <93908182+hanseysong@users.noreply.github.com>
2022-06-19 19:28:41 +0200

    cart updated

---
+++ sbtp_29_bbl(WORK ON THIS)/js/cart.js
@@ -0,0 +1,28 @@
+console.log('here it is')
+
+let removeCartItemButtons = document.getElementsByClassName('remove')
+
+console.log(removeCartItemButtons)
+
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        let buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        console.log('removed')
+        updateCartTotal()
+    })
+}
+
+function updateCartTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-item')[0]
+    let cartRows = document.getElementsByClassName('cart-row')
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i]
+        let priceElement1 = document.getElementsByClassName('product-price-1')[0]
+        let quantityElement = document.getElementsByClassName('product-quantity-1')[0]
+        console.log(priceElement1, quantityElement)
+
+    }
+
+}
